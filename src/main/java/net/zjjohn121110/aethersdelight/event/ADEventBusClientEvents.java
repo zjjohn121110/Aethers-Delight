32996dc1b80b4c3a33c1aea687f473b55a7f732a
zjjohn121110 <dejohn12118@gmail.com>
2025-01-28 20:01:14 -0500

    Refractor

--- src/main/java/net/zjjohn121110/aethersdelight/event/ADEventBusClientEvents.java
+++ src/main/java/net/zjjohn121110/aethersdelight/event/ADEventBusClientEvents.java
@@ -9,7 +9,7 @@ import net.zjjohn121110.aethersdelight.block.entity.AethersDelightBlockEntity;
 import net.zjjohn121110.aethersdelight.block.entity.renderer.HolystoneStoveBlockEntityRenderer;
 
 @EventBusSubscriber(modid = AethersDelight.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-public class ModEventBusClientEvents {
+public class ADEventBusClientEvents {
     @SubscribeEvent
     public static void registerBER(EntityRenderersEvent.RegisterRenderers event) {
         event.registerBlockEntityRenderer(AethersDelightBlockEntity.HOLYSTONE_STOVE.get(), HolystoneStoveBlockEntityRenderer::new);
